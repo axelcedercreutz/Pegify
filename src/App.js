3ac8afdd7cd3b4d69fab10f720574d618188ded1
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2020-03-20 18:07:31 +0200

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <form>
-        <label>
-          Nimi:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Koko:
-          <input type="text" name="size" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      </header>
+      <SignIn/>
     </div>
   );
 }
