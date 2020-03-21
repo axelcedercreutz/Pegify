4bc271eebf6850033c52ee9029e603666b72b6cf
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2020-03-21 07:02:55 +0200

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './SignIn';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
 
 function App() {
+
+  const [ showInventory, setShowInventory ] = useState(false);
+
+  const handleClick = () => {
+    setShowInventory(!showInventory);
+  }
+
   return (
     <div className="App">
-      <SignIn/>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" onClick={handleClick}>
+            {!showInventory ? 'Show Inventory' : 'Add new'}
+          </Button>
+        </Toolbar>
+      </AppBar>
+      {!showInventory ?
+        <SignIn/> :
+        <div>
+          <Typography>Test</Typography>
+        </div>
+      }
     </div>
   );
 }
