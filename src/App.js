5af07225c760115fba93330f217bf91b29583397
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2020-03-23 11:40:05 +0200

    added icons

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Dashboard from './Dashboard';
+import Settings from './Settings';
+import AddIcon from '@material-ui/icons/Add';
+import SettingsIcon from '@material-ui/icons/Settings';
+import DashboardIcon from '@material-ui/icons/Dashboard';
 
 
 function App() {
 
-  const [ showInventory, setShowInventory ] = useState(false);
+  const [showInventory, setShowInventory] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
   const [ inventory, setInventory] = useState([]);
 
@@ -18,22 +23,41 @@ function App() {
     setInventory(newInventory);
   }
 
-  const handleClick = () => {
+  const handleShowInventory = () => {
+    showSettings && setShowSettings(!showSettings);
     setShowInventory(!showInventory);
+  }
+  const handleShowSettings = () => {
+    showInventory && setShowInventory(!showInventory);
+    setShowSettings(!showSettings);
   }
 
   return (
     <div className="App">
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" onClick={handleClick}>
-            {!showInventory ? 'Dashboard' : 'Add new product'}
+          <Button color="inherit" onClick={handleShowInventory}>
+            {
+              !showInventory ?
+              <DashboardIcon/> :
+              <AddIcon />
+            }
+          </Button>
+          <Button color="inherit" onClick={handleShowSettings}>
+            {
+              !showSettings ?
+              <SettingsIcon/> :
+              <AddIcon />
+            }
           </Button>
         </Toolbar>
       </AppBar>
-      {!showInventory ?
-        <NewProduct handleAddClick={(e) => handleAddClick(e)}/> :
-        <Dashboard inventory={inventory} />
+      {!showInventory && !showSettings ?
+          <NewProduct handleAddClick={(e) => handleAddClick(e)}/> :
+        showSettings ?
+          <Settings/> :
+          <Dashboard inventory={inventory} />
+        
       }
     </div>
   );
---
+++ src/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function Settings() {
+  return (
+    <>
+        <Typography>Hello</Typography>
+    </>
+  );
+}
+
+export default Settings;
